c3983d29fdfa658ced1d7dbdb4eca67159044396
allos <sadeosun@axxess.com>
2022-05-28 02:54:40 +0100

    whatsapp clone using dart flutter

---
+++ lib/components/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class ColorConstant {
+  static const lightGreen = Color(0xFF25D366);
+  static const tealGreen = Color(0xFF128C7E);
+  static const tealGreenDark = Color(0xFF075E54);
+  static const blue = Color(0xFF34B7F1);
+}
